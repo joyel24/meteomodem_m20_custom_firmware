3aff78e3dc8f2069968e6c6c735385472d2863fc
joel <joel.martin.2104@gmail.com>
2023-10-14 21:46:50 +0200

    USART1 global interrupt

--- Core/Inc/stm32l0xx_it.h
+++ Core/Inc/stm32l0xx_it.h
@@ -51,6 +51,7 @@ void HardFault_Handler(void);
 void SVC_Handler(void);
 void PendSV_Handler(void);
 void SysTick_Handler(void);
+void USART1_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
 /* USER CODE END EFP */
