e410d1bbf27627980988e188fdd454e91f40cf22
joel <joel.martin.2104@gmail.com>
2023-10-16 03:55:16 +0200

    almost pocsag for hears

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -211,7 +211,13 @@ void TxDATA_test(char bits[]){
 		clearbuffer();
 
 		__HAL_TIM_SET_COUNTER(&htim21, 0);
+		TIM21_increment=0;
 
+		uint32_t test = ( (TIM21_increment * 65535) + __HAL_TIM_GET_COUNTER(&htim21) );
+		//uint32_t test2 = bitNumber*(8000000/1200);
+		sprintf(serialTXbuffer,"%u \n", test);
+		HAL_UART_Transmit(&huart1, serialTXbuffer, sizeof (serialTXbuffer), sizeof (serialTXbuffer));
+		clearbuffer();
 		for (int16_t bitNumber=0; bitNumber < 1120; bitNumber++){
 			TIM21_value = __HAL_TIM_GET_COUNTER(&htim21);
 
@@ -220,23 +226,23 @@ void TxDATA_test(char bits[]){
 			//clearbuffer();
 
 			if (bits[bitNumber] == '0'){
-				//while ( __HAL_TIM_GET_COUNTER(&htim6) < bitNumber*(8000000/1200) ){}
+				while ( ( (TIM21_increment * 65535) + __HAL_TIM_GET_COUNTER(&htim21) ) < bitNumber*(8000000/1200) ){}
 				HAL_GPIO_WritePin(ADF7012_TxDATA_GPIO_Port, ADF7012_TxDATA_Pin, GPIO_PIN_RESET);
 
 				//HAL_UART_Transmit(&huart1, UartBuffOut, strlen(UartBuffOut), 1000);
 				//clearbuffer();
 				//sprintf(serialTXbuffer,"%d", HAL_GPIO_ReadPin(ADF7012_TxDATA_GPIO_Port, ADF7012_TxDATA_Pin));
 				//HAL_UART_Transmit(&huart1, serialTXbuffer, sizeof (serialTXbuffer), sizeof (serialTXbuffer));
-				HAL_Delay(1);
+				//HAL_Delay(1);
 				}
 			else if(bits[bitNumber] == '1'){
 				//HAL_UART_Transmit(&huart1, UartBuffOut, strlen(UartBuffOut), 1000);
-				//while ( __HAL_TIM_GET_COUNTER(&htim6) < bitNumber*(8000000/1200) ){}
+				while ( ( (TIM21_increment * 65535) + __HAL_TIM_GET_COUNTER(&htim21) ) < bitNumber*(8000000/1200) ){}
 				HAL_GPIO_WritePin(ADF7012_TxDATA_GPIO_Port, ADF7012_TxDATA_Pin, GPIO_PIN_SET);
 				//clearbuffer();
 				//sprintf(serialTXbuffer,"%d", HAL_GPIO_ReadPin(ADF7012_TxDATA_GPIO_Port, ADF7012_TxDATA_Pin));
 				//HAL_UART_Transmit(&huart1, serialTXbuffer, sizeof (serialTXbuffer), sizeof (serialTXbuffer));
-				HAL_Delay(1);
+				//HAL_Delay(1);
 
 			}
 			//bitNumber++;
@@ -245,6 +251,7 @@ void TxDATA_test(char bits[]){
 		sprintf(serialTXbuffer,"TxDATA_test() while end\n");
 		HAL_UART_Transmit(&huart1, serialTXbuffer, sizeof (serialTXbuffer), sizeof (serialTXbuffer));
 		clearbuffer();
+		HAL_Delay(2500);
 
 		if ( HAL_GPIO_ReadPin(BUTTON_GPIO_Port, BUTTON_Pin) == 0 ) {
 			sprintf(serialTXbuffer,"\nswitch mode request\n");
@@ -333,6 +340,7 @@ int main(void)
 
 		switch (activeMode)
 		{
+			if (activeMode > maxActiveMode){activeMode=0;}
 			case 0:
 				printstr("case 0");
 				//HAL_GPIO_WritePin(GPS_Heater_LDO_EN_GPIO_Port, GPS_Heater_LDO_EN_Pin, GPIO_PIN_SET); //Enable GPS & Heater
@@ -500,7 +508,7 @@ int main(void)
 				*/
 
 				myspi(0b00000011110001000010000001001100);
-				myspi(setfreq(446100000, 8000000, 0));
+				myspi(setfreq(433920000, 8000000, 0));
 				myspi(0b00000000000000000010110011100010);
 
 				mode3();
